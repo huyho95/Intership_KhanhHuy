4f3d4003fd342fadf2a53e3eccf105f1778f1cbc
KhanhHuyHo <huyho95@gmail.com>
2020-10-28 21:34:12 +0700

    commit

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NzButtonModule } from 'ng-zorro-antd/button';
 
 import { AuthRoutingModule } from './auth-routing.module';
 import { SignInComponent } from './sign-in/sign-in.component';
@@ -10,7 +11,8 @@ import { SignUpComponent } from './sign-up/sign-up.component';
   declarations: [SignInComponent, SignUpComponent],
   imports: [
     CommonModule,
-    AuthRoutingModule
+    AuthRoutingModule,
+    NzButtonModule
   ]
 })
 export class AuthModule { }
